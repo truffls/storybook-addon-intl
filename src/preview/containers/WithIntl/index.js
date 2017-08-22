6d19183faafb908c16ddf0c74c568d0797b07157
Joao Ribeiro <joao.ribeiro@addisongloballtd.com>
2017-08-22 06:28:50 +0200

    [null issue] fix tests

--- src/preview/containers/WithIntl/index.js
+++ src/preview/containers/WithIntl/index.js
@@ -10,7 +10,7 @@ class WithIntl extends React.Component {
         super(props);
 
         this.state = {
-            locale: props.intlConfig.defaultLocale
+            locale: props.intlConfig.defaultLocale || null
         };
 
         this.setLocale = this.setLocale.bind(this);
